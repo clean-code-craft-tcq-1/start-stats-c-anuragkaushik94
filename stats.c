f817ecd55b1ce7f54e90ef381049d9fbeb53b048
anuragkaushik94 <77480798+anuragkaushik94@users.noreply.github.com>
2021-01-27 22:55:07 +0530

    Updated the file and trying 1st commit

--- stats.c
+++ stats.c
@@ -1,10 +1,11 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+struct compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
     s.average = 0;
     s.min = 0;
     s.max = 0;
+    return s;
 }
 
 int emailAlertCallCount = 0;
