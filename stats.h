21df0500eb36e4ac66a7badad2c8ff609e14a441
anuragkaushik94 <77480798+anuragkaushik94@users.noreply.github.com>
2021-01-28 18:17:00 +0530

    updated the structure name

--- stats.h
+++ stats.h
@@ -1,5 +1,12 @@
+typedef struct 
+{
+	float average;
+	float max;
+	float min;
 
-struct compute_statistics(const float* numberset, int setlength);
+}computedStats;
+
+struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
 void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
